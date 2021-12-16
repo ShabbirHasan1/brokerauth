c1681ef7ab76add25fea3facfb0218e80b3e7525
pratikbhopal@gmail.com <praSMA@1>
2021-12-16 12:51:51 +0530

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='brokerauth',
-    version='1.1',
+    version='1.2',
     author='Pratik Bhopal',
     author_email='pratikbhopal@gmail.com',
     description='Testing installation of Package',
